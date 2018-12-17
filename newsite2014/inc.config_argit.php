153fa5792cb83400b1ce4d83abaa1347438b3577
lizardking <jose.sosa88@gmail.com>
2018-12-17 14:36:17 -0300

    sacamos el var_dump del buscar_dni

--- newsite2014/inc.config_argit.php
+++ newsite2014/inc.config_argit.php
@@ -1,12 +1,13 @@
 <?php
 error_reporting(0);
 date_default_timezone_set('America/Argentina/Buenos_Aires');
-$dbuser = "ktkkbfac";
-$dbpassword = "3Jo8Ngd00l";
+$dbuser = "root";
+$dbpassword = "";
 $dbhost = "localhost";
 $dbname = "inscribite_base";
 $mysqli = new mysqli($dbhost, $dbuser, $dbpassword, $dbname);
-$general_path = "http://inscribitetest.rhind.com.ar";
+
+$general_path = "http://127.0.01:88/inscribite_repositorio";
 $unirse_path = $general_path."unirse.php";
 define("pftables", "inscribite_");
 define("localPass", "nqvcwenj872k1ui202gg4ff27xjb");
@@ -15,3 +16,8 @@ $smtpHost = '';
 $smtpPuerto = '';
 $smtpUsername = '';
 $smtpPassword = '';
+
+if ($mysqli->connect_error) {
+    die('Error de Conexión (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+}
